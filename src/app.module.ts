61fd4bbdcd6d9cf3b8fa0c53f9e6a17939d51bd1
nam9nine <southgyuios19@gmail.com>
2025-05-05 16:07:22 +0900

    chore(project): 초기 프로젝트 구조 설정 [#1]

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TrafficModule } from '@traffic/traffic.module';
+import { AuthModule } from '@auth/auth.module';
+import { UsersModule } from '@users/users.module';
+import { AlertsModule } from '@alerts/alerts.module';
+import { AgentModule } from '@agent/agent.module';
+import { CalendarModule } from '@calendar/calendar.module';
+
+@Module({
+  imports: [
+    // 환경변수 설정
+    ConfigModule.forRoot({ isGlobal: true }),
+    // TypeORM 설정 (PostgreSQL)
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST!,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME!,
+      password: process.env.DB_PASSWORD!,
+      database: process.env.DB_DATABASE!,
+      autoLoadEntities: true,
+      synchronize: true, // 개발 환경에서만 true로 설정
+    }),
+    // Application Modules
+    TrafficModule,
+    CalendarModule,
+    AgentModule,
+    AlertsModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
