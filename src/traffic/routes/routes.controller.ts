a87f703e976adf7dc0723a9ce441c05a874b005a
nam9nine <southgyuios19@gmail.com>
2025-05-23 23:56:57 +0900

    feat(traffic/routes): CAR mode추가 및 응답 형식 변경 [#10]

--- src/traffic/routes/routes.controller.ts
+++ src/traffic/routes/routes.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RouteRequestDto } from './dto/route-request.dto';
-import { RoutesResponseDto, RouteDto } from './dto/route-info.dto';
+import { AllRoutesDataDto } from './dto/all-routes-response.dto';
 import { plainToInstance } from 'class-transformer';
 
 @Controller('traffic/routes')
@@ -9,14 +9,13 @@ export class RoutesController {
   constructor(private readonly routesService: RoutesService) {}
 
   @Post()
-  async getAllRoutes(@Body() dto: RouteRequestDto): Promise<RoutesResponseDto> {
-    const data = await this.routesService.getAllRoutes(dto.fromAddress, dto.toAddress);
-    return plainToInstance(RoutesResponseDto, data);
-  }
-
-  @Post('optimal')
-  async getOptimalRoute(@Body() dto: RouteRequestDto): Promise<RouteDto> {
-    const data = await this.routesService.getOptimalRoute(dto.fromAddress, dto.toAddress);
-    return plainToInstance(RouteDto, data);
+  async getAllRoutes(
+    @Body() dto: RouteRequestDto,
+  ): Promise<AllRoutesDataDto> {
+    const data = await this.routesService.getAllRoutes(
+      dto.fromAddress,
+      dto.toAddress,
+    );
+    return plainToInstance(AllRoutesDataDto, data);
   }
 } 
