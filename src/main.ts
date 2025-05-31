eb3a5a6bb208d2db49c69683608632bbaf0579f0
nam9nine <southgyuios19@gmail.com>
2025-05-31 12:05:22 +0900

    chore(log): log util 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { inspect } from 'util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  inspect.defaultOptions.depth = null;
+  const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log', 'debug', 'verbose'] });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
