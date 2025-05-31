ed0efc1a3cf9c75a86f82437997486bd9f35e13f
nam9nine <southgyuios19@gmail.com>
2025-05-31 21:32:50 +0900

    fix: polyline를 namespace import로 변경

--- src/traffic/its/its.controller.ts
+++ src/traffic/its/its.controller.ts
@@ -4,7 +4,7 @@ import { RoutesService } from '../routes/routes.service';
 import { ItsService } from './its.service';
 import { LinkMappingService } from './link-mapping.service';
 import { SECTION_INFO } from './section-map';
-import polyline from '@mapbox/polyline';
+import * as polyline from '@mapbox/polyline';
 
 @Controller('traffic/its')
 export class ItsController {
--- src/traffic/routes/routes.service.ts
+++ src/traffic/routes/routes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import polyline from '@mapbox/polyline';
+import * as polyline from '@mapbox/polyline';
 import { firstValueFrom } from 'rxjs';
 import { TmapService } from '../tmap/tmap.service';
 import { TagoService } from '../tago/tago.service';
